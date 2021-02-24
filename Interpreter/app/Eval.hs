dbcb6d9060ab3196ec583ef67fbaf90218f566ef
Victor <victor.z@ciencias.unam.mx>
2021-02-24 13:29:30 -0600

    Backup

--- Interpreter/app/Eval.hs
+++ Interpreter/app/Eval.hs
@@ -10,6 +10,15 @@ import Text.ParserCombinators.Parsec hiding (State)
 import Numeric (showIntAtBase)
 import Data.Char (intToDigit)
 
+-- Maximum number of steps to execute for each program.
+maxSteps = 10000
+
+-- File containing programs to run.
+programs = "programs.txt"
+
+-- Folder containing program files.
+program_files_loc = "programs/"
+
 --A memory state using lists
 type StateL = [(Int, Integer)]
 
@@ -77,19 +86,6 @@ simplBoolProgram (While b p)
         p' = simplBoolProgram p
 simplBoolProgram x = x
   
---Defining a simple eval function
-eval :: Program -> StateL -> StateL
-eval NoHalt s = replace 0 s (-1) 
-eval Skip s = s
-eval (Assign (Loc x) a) s = replace x s (evalArit a s)
-eval (Concat p1 p2) s = let sNew = (eval p1 s)
-                        in (eval p2 sNew)
-eval (If b p1 p2) s = if (evalBool b s) then (eval p1 s)
-                      else (eval p2 s)
-eval (While b p) s = if (evalBool b s) then let sNew = (eval p s)
-                                          in (eval (While b p) sNew)
-                     else s
-
 --Evaluation function with optional halting parameter.
 evalWH :: Program -> StateL -> Int -> (StateL, Int)
 evalWH p s halt = if (halt <= 0)
@@ -114,14 +110,6 @@ evalWHAux (While b p) s halt = let p1 = (evalBoolWH b s halt)
                                   else (s, (snd p1) - 1)
 
  
--- Evaluates an arithmetic expression
-evalArit :: Arit -> StateL -> Integer
-evalArit (In n) _ = n
-evalArit (Mem (Loc x)) s = (getValue s x)   
-evalArit (Plus a1 a2) s = (evalArit a1 s) + (evalArit a2 s)
-evalArit (Minus a1 a2) s = Util.natSub (evalArit a1 s) (evalArit a2 s)
-evalArit (Times a1 a2) s = (evalArit a1 s) * (evalArit a2 s)
-
 -- Evaluates an arithmetic expression with a halting parameter.
 evalAritWH :: Arit -> StateL -> Int -> (Integer, Int)
 evalAritWH (In n) _ halt = (n, halt)
@@ -135,16 +123,6 @@ evalAritWH (Minus a1 a2) s halt = let p1 = (evalAritWH a1 s halt)
 evalAritWH (Times a1 a2) s halt = let p1 = (evalAritWH a1 s halt)
                                   in let p2 = (evalAritWH a2 s (snd p1))
                                      in ((fst p1) * (fst p2), (snd p2) -1)
-
--- Evaluates a boolean expression
-evalBool :: BoolExp -> StateL -> Bool
-evalBool T _ = True
-evalBool F _ = False
-evalBool (Equals a1 a2) s = (evalArit a1 s) == (evalArit a2 s)
-evalBool (Lessthan a1 a2) s = (evalArit a1 s) < (evalArit a2 s)
-evalBool (Not b) s = (not (evalBool b s))
-evalBool (Or b1 b2) s = ((evalBool b1 s) || (evalBool b2 s))
-evalBool (And b1 b2) s = ((evalBool b1 s) && (evalBool b2 s))
 
 -- Evaluates a boolean expression with halting parameter.
 evalBoolWH :: BoolExp -> StateL -> Int -> (Bool, Int)
@@ -197,42 +175,28 @@ getStringFromTuple (n, m)
   | otherwise = replicate (m - (integerLog2 n) - 1) '0' ++ stringn
   where stringn = showIntAtBase 2 intToDigit n ""
                                                       
---Executes the program and returns a value.
---This function receives a function that gets the return value from the state.
-executeProgram :: Program -> (StateL -> String) -> String
-executeProgram p f = f $ eval p []
-
--- Executes the program with the option to halt if, after a number of steps,
--- the program hasn't halted.
-executeProgramWH :: Program -> Int -> (StateL -> Integer) -> Integer
-executeProgramWH p halt f = f $ fst $ evalWH p [] halt
+{- Executes the program with the option to halt if, after a number of steps,
+ - the program hasn't halted.
+ - First parameter is the program.
+ - Second parameter is the maximum number of steps to execute. If the number is 0, we
+ - default to the value established by maxSteps.
+ - Third parameter is a function that returns the program's output. -}
+executeProgram :: Program -> Int -> (StateL -> String) -> String
+executeProgram p halt f = let halt' = if halt < 0
+                                      then maxSteps
+                                      else halt
+                          in f $ fst $ evalWH p [] halt'
 
 -- Versión sin arreglos.
 main :: IO ()
-main = do handle <- openFile "example.while" ReadMode
-          c <- hGetContents handle
-          case parse numberedProgramWithHalt "(stdin)" c of
-            Left e -> do putStrLn "Error parsing input:"
-                         print e
-            Right r -> do
-              print $ "Program number: " ++ (show $ Util.fst r)
-              print $ "Halting parameter: " ++ (show $ Util.snd r)
-              print $ "Program: " ++ (show $ Util.thrd r)
-
--- main :: IO ()
--- main = do c <- getContents
---           let l = head (lines c)
---           case parse parsewoFirstLine "(stdin)" c of
---             Left e -> do putStrLn "Error parsing input:"
---                          print e
---             Right r -> appendFile "output.txt" ("\n" ++ l ++ " - " ++ (executeProgram r (getReturnValue)))
-            
-
--- Versión sin y con detención en 5 pasos.
--- main :: IO ()
--- main =
---     do c <- getContents
---        case parse program "(stdin)" c of
---             Left e -> do putStrLn "Error parsing input:"
---                          print e
---             Right r -> print (executeProgramWH r 16 (getReturnValue 1))
+main = do hanP <- openFile programs ReadMode
+          c <- hGetContents hanP
+          let programs = lines c
+          print programs
+          -- case parse numberedProgramWithHalt "(stdin)" c of
+          --   Left e -> do putStrLn "Error parsing input:"
+          --                print e
+          --   Right r -> do
+          --     let res = executeProgram (Util.thrd r) (Util.snd r) getReturnValue
+              
+              
