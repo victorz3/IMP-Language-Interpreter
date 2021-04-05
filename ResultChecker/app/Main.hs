bc98470b79e6ddda366d6a6ab3c943d1efcdeca2
Victor <victor.z@ciencias.unam.mx>
2021-04-05 18:00:57 -0500

    feat: adds function to find the result of a program
    We needed a function to find the result of a program from an
    outputs file. The functions findResult and getProgramResult were
    added to the ResultChecker application for this purpose.

--- ResultChecker/app/Main.hs
+++ ResultChecker/app/Main.hs
@@ -2,6 +2,7 @@
  - correct.
  - Author: Victor Zamora -}
 
+-- System libraries.
 import System.IO
 import Control.Monad
 import qualified Data.ByteString.Lazy as LB
@@ -9,48 +10,82 @@ import Data.Digest.Pure.MD5
 import System.Random
 import Data.Sort
 import Data.Time.Clock
-import Util
+
+-- Our libraries.
+import qualified Util
+import qualified ProgramHandler
 
 -- Name of file with the hash code.
 hashFile = "hash.txt"
 -- Name of file containing the results of execution.
-filename = "results.txt"
+results = "outputs.txt"
 -- File containing numbers for executed programs.
 programs = "programs.txt"
 -- Number of files to cross-check
 checks = 5
 
+{- | 'findResult' finds the result of a specific program within
+     a '[String]' containing lines with various programs'
+     results.
+     It has two parameters. The first parameter contains the
+     lines with results and the second parameter contains the
+     number of the specific program we're looking for.
+-}
+findResult :: [String] -> String -> String
+findResult [] p = error ("No entry for program" ++ p)
+findResult (s:l) p = let w = words s in
+                       if p == head w
+                       -- Last element of the line is the
+                       -- program's result
+                       then head $ tail $ w
+                       else findResult l p
+  
+-- ^Gets a program's result from the 'results' file.
+getProgramResult :: String -> IO String
+getProgramResult p = do
+-- First, look for the program's number.
+-- Open program.
+  program <- ProgramHandler.openProgram p
+-- Parse the program.
+  let num = head $ lines $ program
+  rHandle <- openFile results ReadMode
+  contents <- hGetContents rHandle
+  let l = lines contents
+  return $ findResult l p
+
 {- Checks the result of a program.
  - First parameter is the program.
  - Second parameter is the expected result.
- - This function returns true if the execution of the program returns the expected
- - result and false otherwise. -}
+ - This function returns true if the execution of the program
+ - returns the expected result and false otherwise. -}
 checkProgramResult :: String -> String -> Bool
-checkProgramResult p r = r == ()
-
+checkProgramResult p r = r == r
 
 main :: IO ()
 main = do
-  {- First, get value of hash. This step might not be necessary depending
-   - on how hash is received. -}
-  handle1 <- openFile hashFile ReadMode
-  c <- hGetContents handle1
-  let hash = take ((length c) - 1) c
-  -- Now read file bytes.
-  fileContent <- LB.readFile filename 
-  -- Compute md5
-  let hash2 = md5 fileContent
-  -- Check if hashes match
-  when (hash /= show hash2) (error "File has been corrupted")
-  {- Now execute checks
-   - First get the list of executed programs. -}
-  handle2 <- openFile programs ReadMode
-  prog <- hGetContents handle2
-  let programs = lines prog
-  -- Get seed from current time.
-  time <- getCurrentTime >>= return . utctDayTime
-  let seed = floor $ toRational $ time
-  let sample = randomSample seed checks programs
-  -- Now, check every program from the sample.
+  r <- getProgramResult "3"
+  print r
+  -- -- First, get value of hash. This step might not be necessary
+  -- -- depending on how hash is received.
+  -- handle1 <- openFile hashFile ReadMode
+  -- c <- hGetContents handle1
+  -- let hash = take ((length c) - 1) c
+  -- -- Now read file bytes.
+  -- fileContent <- LB.readFile results 
+  -- -- Compute md5
+  -- let hash2 = md5 fileContent
+  -- -- Check if hashes match
+  -- when (hash /= show hash2) (error "File has been corrupted")
+  -- -- Now execute checks.
+  -- -- First get the list of executed programs. 
+  -- handle2 <- openFile programs ReadMode
+  -- prog <- hGetContents handle2
+  -- let programs = lines prog
+  -- -- Get seed from current time.
+  -- time <- getCurrentTime >>= return . utctDayTime
+  -- let seed = floor $ toRational $ time
+  -- let sample = Util.randomSample seed checks programs
+  -- print sample
+  -- -- Now, check every program from the sample.
   
   
