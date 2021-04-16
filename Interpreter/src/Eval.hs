fc2ef31889636a8baa026851dbc5a25bf247d979
Victor <victor.z@ciencias.unam.mx>
2021-04-15 19:14:52 -0500

    refactor: moves functions to compute output to a new module 'Output'

--- Interpreter/src/Eval.hs
+++ Interpreter/src/Eval.hs
@@ -11,6 +11,7 @@ import Data.Maybe
 import Language
 import Math.NumberTheory.Logarithms
 import Numeric (showIntAtBase)
+import Output
 import qualified Util
 
 {- | 'maxSteps' contains the default value for the maximum number of steps
@@ -206,40 +207,6 @@ evalBoolWH (And b1 b2) s halt = let p1 = (evalBoolWH b1 s halt)
                                 in if (not (fst p1))
                                    then (False, (snd p1) -1)
                                    else (evalBoolWH b2 s ((snd p1) - 1))
-
-{- | 'natIntoString' takes a natural number "n" and obtains the nth binary
-     string in canonical order.
-     This function returns a 'Tuple' where the first element is the binary
-     number corresponding to the string, and the second parameter is the
-     number of bits to use in the binary number's representation as a
-     'String'.
-     For example, if the first element of the tuple is 2 and the second
-     element is 8, we would be representing 10 (2 in binary) in 8 bits,
-     which would give us the 'String' "00000010".
-     If the parameter passed to this function is negative, the tuple
-     '(-1, 0)' is returned, indicating an error.
--}
-
-natIntoString :: Integer -> (Integer, Int)
-natIntoString 0 = (0, 1)
-natIntoString n = if n > 0
-                  then let l = integerLog2 (n+2)
-                       in let c = n - (2^l) + 2
-                          in (c, l)
-                    else (-1, 0)
-
-{- | 'getStringFromTuple' transforms the tuple returned by 'natIntoString'
-     into a 'String' composed of 0's and 1's.
-     When the error tuple ('(-1, 0)' by our convention) is passed to this
-     function, the 'String' "err" is returned, indicating an error. In
-     practice, this indicates that the program did not halt.
--}
-getStringFromTuple :: (Integer, Int) -> String
-getStringFromTuple (n, m)
-  | m <= 0 = "err"
-  | n == 0 = replicate m '0'
-  | otherwise = replicate (m - (integerLog2 n) - 1) '0' ++ stringn
-  where stringn = showIntAtBase 2 intToDigit n ""
  
 {- | 'getReturnValue' takes a 'State', obtains the value "i" of the 0th
      register, and returns the ith binary string in canonical order, where
